af8213cf107276f7a90844a3acc4a6ffc9d8b295
Song Jaehak <master@hpi.cc>
2016-08-03 14:56:42 +0900

    함수 테스트 중

--- rplugin/python3/simple_plugin.py
+++ rplugin/python3/simple_plugin.py
@@ -6,11 +6,11 @@ class SimplePlugin(object):
     def __init__(self, nvim):
         self.nvim = nvim
 
-    @neovim.function('helloworld', sync=True)
+    @neovim.function('HelloWorld', sync=True)
     def helloworld(self, args):
         self.nvim.command('echo "hello world!"')
 
 
     @neovim.command('HelloWorld', range='', nargs='*')
     def helloworld_command(self, args, range):
-        self.nvim.call('helloworld()')
+        self.nvim.call('HelloWorld()')
