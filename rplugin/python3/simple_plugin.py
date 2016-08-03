2781d288f6920fb92afc231d951eb6603777c5d6
Song Jaehak <master@hpi.cc>
2016-08-03 14:53:04 +0900

    function과 command 분리

--- rplugin/python3/simple_plugin.py
+++ rplugin/python3/simple_plugin.py
@@ -6,6 +6,11 @@ class SimplePlugin(object):
     def __init__(self, nvim):
         self.nvim = nvim
 
-    @neovim.command('HelloWorld', range='', nargs='*')
-    def helloworld(self, args, range):
+    @neovim.function('helloworld')
+    def helloworld(self, args):
         self.nvim.command('echo "hello world!"')
+
+
+    @neovim.command('HelloWorld', range='', nargs='*')
+    def helloworld_command(self, args, range):
+        self.nvim.helloworld()
