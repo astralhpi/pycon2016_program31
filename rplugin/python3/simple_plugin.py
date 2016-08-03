100f60b12433812dfb192266fb7e835b47fc16ed
Song Jaehak <master@hpi.cc>
2016-08-03 15:39:27 +0900

    rpc_export 테스트

--- rplugin/python3/simple_plugin.py
+++ rplugin/python3/simple_plugin.py
@@ -6,11 +6,15 @@ class SimplePlugin(object):
     def __init__(self, nvim):
         self.nvim = nvim
 
-    @neovim.function('HelloWorld', sync=True)
+    @neovim.rpc_export("helloworld")
+    def helloworld_rpc(self, *args):
+        self.nvim.call('helloworld')
+
+    @neovim.function('helloworld', sync=True)
     def helloworld(self, args):
         self.nvim.command('echo "hello world!"')
 
 
     @neovim.command('HelloWorld', range='', nargs='*')
     def helloworld_command(self, args, range):
-        self.nvim.call('HelloWorld')
+        self.nvim.call('helloworld')
